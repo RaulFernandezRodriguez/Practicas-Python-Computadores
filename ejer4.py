78e82b4a272dc98a378f1b86519f6aae83fa9ed2
RaulFernandezRodriguez <rfernr08@estudiantes.unileon.es>
2023-03-27 20:05:48 +0200

    u

--- ejer4.py
+++ ejer4.py
@@ -1,12 +1,11 @@
-
 class volumen():
-    def volumen_cono(altura, radio):
+    def volumen_cono(self, altura, radio):
         result = altura * radio * radio * 3.14 / 3
         return result
-    def volumen_ortoedro(altura, lado1, lado2):
+    def volumen_ortoedro(self, altura, lado1, lado2):
         result = altura * lado1 * lado2
         return result
-    def menu():
+    def menu(self):
         print("1-Cono\n")
         print("2-Ortoedro\n")
         print("3-Salir\n")
@@ -15,17 +14,18 @@ class volumen():
             while select<=0 or select > 3:
               select = int(input("Elige que figura quieres calcular su volumen: "))
         else:
-                if select == 1:
-                    radio = float(input("Escribe el radio del cono: "))
-                    altura = float(input("Escribe la altura del cono: "))
-                    result = volumen_cono(altura, radio)
-                    print(result)
-                elif select == 2:
-                    lado1 = float(input("Escribe un lado del ortoedro: "))
-                    lado2 = float(input("Escribe el otro: "))
-                    altura = float(input("Escribe tambien la altura del ortoedro: "))
-                    print(volumen_ortoedro(altura, lado1, lado2))
-                elif select == 3:
-                    print("Saliste del programa")
-
-    menu()
+            if select == 1:
+                radio = float(input("Escribe el radio del cono: "))
+                altura = float(input("Escribe la altura del cono: "))
+                result = self.volumen_cono(altura, radio)
+                print(result)
+            elif select == 2:
+                lado1 = float(input("Escribe un lado del ortoedro: "))
+                lado2 = float(input("Escribe el otro: "))
+                altura = float(input("Escribe tambien la altura del ortoedro: "))
+                result = self.volumen_ortoedro(altura, lado1, lado2) 
+                print(result)
+            elif select == 3:
+                print("Saliste del programa")
+volumen_obj = volumen()
+volumen_obj.menu()
--- ejer5.py
+++ ejer5.py
@@ -1,15 +1,15 @@
 
 class pares():
     def imprime_pares_intervalos(num1, num2):
-        while num1 < num2:
-            if (num2 % 2) == 0:
+        while (num1 < num2):
+            if(num2 % 2) == 0:
                 print(num2)
                 print("")
-            num2 -1
+            num2 =num2 -1
     num1 = int(input("Introduce un numero inicial:"))
     num2 = int(input("Introduce un numero limite:"))
     if(num2 < num1):
-        while num2 < num1:
+        while (num2 < num1):
             print("Error, reintroduce los valores")
             num1 = int(input("Introduce un numero inicial:"))
             num2 = int(input("Introduce un numero limite:"))
